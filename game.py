59d52d65c8659cbba3967887c391b6a580fbd32a
Anton <mrtony2371@gmail.com>
2019-10-13 23:47:36 +0300

    game test

--- game.py
+++ game.py
@@ -2,12 +2,14 @@ import pygame
 
 pygame.init()
 
-
 window = pygame.display.set_mode((800,600))
+
 pygame.display.set_caption("Game")
 clock = pygame.time.Clock()
 
 running = True
+y = 100
+x = 100
 
 while running:
     clock.tick(15)
@@ -17,7 +19,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    pygame.draw.aaline(window, (255,255,255) ,[100,100], [560, 700])
+    pygame.draw.rect(window, (0,255,55),(400,200,63.5,88.8))
+
+    pygame.display.update()
 
 
 pygame.quit()
