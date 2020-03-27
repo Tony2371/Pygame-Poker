cd7eb830b77d35fcefe658d1335195384199cf30
Anton <mrtony2371@gmail.com>
2020-03-28 00:36:57 +0300

    The game is back on!

--- poker_main.py
+++ poker_main.py
@@ -1,6 +1,4 @@
 from random import shuffle
-from operator import itemgetter
-from itertools import groupby
 import pygame
 
 
@@ -10,7 +8,6 @@ class Card(object):
         self.suit = suit
         self.name = name
         self.showing = False
-        
     def __repr__(self):
         if self.showing and self.value <=10:
             return str(self.value)+" of "+self.suit
@@ -19,20 +16,11 @@ class Card(object):
         else:
             return "Card"
 
-    #---------- GUI functions ----------
-    def draw_card(self):
-        if self.showing:
-            self.card_image = pygame.transform.scale(pygame.image.load("deck_images/{0}{1}.jpg".format(self.value,self.suit)),(128//3, 178//3))
-            return self.card_image
-        else:
-            self.card_image = pygame.transform.scale(pygame.image.load("deck_images/back.jpg"),(128//3, 178//3))
-            return self.card_image
 
 class StandardDeck(list):
     def __init__(self):
         self.shuffled = False
-        suits = ["s","h","d","c"]
-        #suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
+        suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
         values = {
         	    "Two":2,
         	    "Three":3,
@@ -68,8 +56,6 @@ class Player(object):
         self.suit_list = []
         self.chip_amount = 1000
         self.current_bet = 0
-        self.ingame = True
-        self.made_action = False
 
         self.combination = [0,0,0]
         # combination takes 3 arguments
@@ -77,61 +63,47 @@ class Player(object):
         # second - highest card value in combination
         # third - second high card (for two pairs and Full house)
         self.kicker = [0,0,0]
-        self.comb_names = {
-            1:"High Card",
-            2:"Pair",
-            3:"Two pairs",
-            4:"Three of a kind",
-            5:"Straight",
-            6:"Flush",
-            7:"Full house",
-            8:"Four of a kind",
-            9:"Straight flush"}
-
-        # GUI variables and functions
-        self.surface = pygame.Surface(((128//3)*2+30, 200))
-
-    def draw_player_surface(self, main_window, coordinates_tuple, players_list):
-        main_window.blit(self.surface,(coordinates_tuple[0],coordinates_tuple[1]))
-        self.surface.fill((50,50,50))
-        pygame.draw.rect(self.surface, (255, 255, 255), self.surface.get_rect(), 3)
-        pygame.draw.line(self.surface,(255,255,255),[0,self.surface.get_height()*0.85],[self.surface.get_width(),self.surface.get_height()*0.85],3)
-        if self.ingame:
-            self.surface.blit(self.hand[0].draw_card(), (10, 10))
-            self.surface.blit(self.hand[1].draw_card(), (20 + 128 // 3, 10))
-
-        f1 = pygame.font.Font(None,16)
-        if self.ingame:
-            line_1 = f1.render("Player {0}".format(self.name), 1, (255, 255, 255))
-            line_2 = f1.render("Chips: {0}".format(self.chip_amount), 1, (255, 255, 255))
-            line_3 = f1.render("{0} of {1}".format(self.comb_names[self.combination[0]], self.combination[1]),1,(255,255,255))
-
-            line_5 = f1.render("Current bet:{0}".format(self.current_bet),1, (150,255,150))
-        
-            self.surface.blit(line_1, (10, 10 * 2 + 178 // 3))
-            self.surface.blit(line_2, (10, 10 * 4 + 178 // 3))
-            self.surface.blit(line_3, (10, 10 * 6 + 178 // 3))
-            
-            self.surface.blit(line_5, (10,self.surface.get_height()-20))
-        if not self.ingame:
-            fold_text =  line_5 = f1.render("Player folded",0, (225,20,20))
-            self.surface.blit(fold_text, (10,self.surface.get_height()-23))
-
 
     def __repr__(self):
         return str("Player "+self.name)
 
-    def straight_eval(self,card_list):
-        data_set = list(set(card_list))
-        ranges = []
-        for k, g in groupby(enumerate(data_set), lambda x:x[0]-x[1]):
-            group = list(map(itemgetter(1),g))
-            ranges.append(range(group[0],group[-1]))
-            if any(True for x in [len(a) for a in ranges] if x >= 4): 
-                return max([x for x in ranges if len(x) >= 4][0])+1
+    def straight_eval(self,input_list):
+        card_list = sorted(input_list)
+        if len(card_list) == 7:
+            if card_list[4] == card_list[3]+1 and card_list[3] == card_list[2]+1 and card_list[2] == card_list[1]+1 and card_list[1] == card_list[0]+1:
+                return card_list[4]
+            elif card_list[5] == card_list[4]+1 and card_list[4] == card_list[3]+1 and card_list[3] == card_list[2]+1 and card_list[2] == card_list[1]+1:
+                return card_list[5]
+            elif card_list[6] == card_list[5]+1 and card_list[5] == card_list[4]+1 and card_list[4] == card_list[3]+1 and card_list[3] == card_list[2]+1:
+                return card_list[6]
             else:
                 return 0
-   
+        elif len(card_list) == 6:
+            if card_list[4] == card_list[3]+1 and card_list[3] == card_list[2]+1 and card_list[2] == card_list[1]+1 and card_list[1] == card_list[0]+1:
+                return card_list[4]
+            elif card_list[5] == card_list[4]+1 and card_list[4] == card_list[3]+1 and card_list[3] == card_list[2]+1 and card_list[2] == card_list[1]+1:
+                return card_list[5]
+            else:
+                return 0
+        elif len(card_list) == 5:
+            if card_list[4] == card_list[3]+1 and card_list[3] == card_list[2]+1 and card_list[2] == card_list[1]+1 and card_list[1] == card_list[0]+1:
+                return card_list[4]
+            else:
+                return 0
+        else:
+            return 0
+
+#   def straight_eval(self,card_list):
+#       data_set = list(set(card_list))
+#       ranges = []
+#       for k, g in groupby(enumerate(data_set), lambda x:x[0]-x[1]):
+#           group = list(map(itemgetter(1),g))
+#           ranges.append(range(group[0],group[-1]))
+#           if any(True for x in [len(a) for a in ranges] if x >= 4): 
+#               return max([x for x in ranges if len(x) >= 4][0])+1
+#           else:
+
+
     def straight_flush_eval(self, board, suits):
         test = []
         if self.straight_eval(board) != 0 and 5 in [suits.count(x) for x in suits]:
@@ -186,7 +158,7 @@ class Player(object):
         elif 4 in self.count_list:
             self.combination[0] = 8
             self.combination[1] = self.val_list[self.count_list.index(4)]
-            self.kicker[0] = max([x for x,y in zip(self.val_list,self.count_list) if y == 1])
+            #self.combination[2] = max([x for x,y in zip(self.val_list,self.count_list) if y == 1])
            
             #7 - Full House
         elif 3 in self.count_list and 2 in self.count_list:
@@ -221,25 +193,27 @@ class Player(object):
             self.combination[0] = 3
             self.combination[1] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 2])[-1]
             self.combination[2] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 2])[-3]
-            self.kicker[0] = max([x for x,y in zip(self.val_list,self.count_list) if y == 1])
+            if self.count_list.count(2) == 6:
+                self.kicker[0] = max([sorted([x for x,y in zip(self.val_list,self.count_list) if y == 2])[0],
+                    sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[0]])
+            else:
+                self.kicker[0] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-1]
 
             #2 - One Pair
         elif len(self.val_list) != len(set(self.val_list)):
             self.combination[0] = 2
             self.combination[1] = self.val_list[self.count_list.index(2)]
-            if len(self.val_list) >= 5:
-                self.kicker[0] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-1]
-                self.kicker[1] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-2]
-                self.kicker[2] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-3]
+            self.kicker[0] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-1]
+            self.kicker[1] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-2]
+            self.kicker[2] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-3]
         
             #1 - High Card
         else:
             self.combination[0] = 1
             self.combination[1] = max(self.val_list)
-            if len(self.val_list) >= 5:
-                self.kicker[0] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-1]
-                self.kicker[1] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-2]
-                self.kicker[2] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-3]
+            self.kicker[0] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-1]
+            self.kicker[1] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-2]
+            self.kicker[2] = sorted([x for x,y in zip(self.val_list,self.count_list) if y == 1])[-3]
 
     def bet(self, bet_amount, board):
         self.chip_amount -= bet_amount
@@ -254,57 +228,23 @@ class Player(object):
     # 3 - bet > N (raise)
     def decision(self, action,players,board):
         if action == "fold":
-            self.ingame = False
-            self.current_bet = 0
-            #self.hand = None
+            self.hand = None
             print(self,"folded")
         elif action == "call":
-            print(self,"calls for {0} chips".format(max([x.current_bet for x in players])-self.current_bet))
-            self.bet(max([x.current_bet for x in players])-self.current_bet,board)
-            self.made_action = True
+            print(self,"calls for {0} chips".format(max(x.current_bet for x in players)-self.current_bet))
+            self.bet(max(x.current_bet for x in players)-self.current_bet,board)
         elif action == "raise":
             raise_amount = int(input("Raise for:"))
             self.bet(raise_amount,board)
             print(self,"raises for {0} chips".format(raise_amount))
-            self.made_action = True
         else:
             print("Enter valid action!")
+
    
 class StandardBoard(list):
     def __init__(self):
         self.bank = 0
         self.game_round = -1
-        self.street = "preflop"
-
-        # GUI variables and functions
-        self.surface = pygame.Surface((270,140))
-
-    def draw_board_surface(self, main_window):
-        main_window.blit(self.surface,((800-self.surface.get_width())//2,230))
-        self.surface.fill((15,45,45))
-        pygame.draw.rect(self.surface, (50, 50, 190), self.surface.get_rect(), 3)
-        if len(self) == 3:
-            self.surface.blit(self[0].draw_card(), (10, 70))
-            self.surface.blit(self[1].draw_card(), (20 + (128 // 3), 70))
-            self.surface.blit(self[2].draw_card(), (30 + 2*(128 // 3), 70))
-        if len(self) == 4:
-            self.surface.blit(self[0].draw_card(), (10, 70))
-            self.surface.blit(self[1].draw_card(), (20 + (128 // 3), 70))
-            self.surface.blit(self[2].draw_card(), (30 + 2*(128 // 3), 70))            
-            self.surface.blit(self[3].draw_card(), (40 + 3*(128 // 3), 70))
-        if len(self) == 5:
-            self.surface.blit(self[0].draw_card(), (10, 70))
-            self.surface.blit(self[1].draw_card(), (20 + (128 // 3), 70))
-            self.surface.blit(self[2].draw_card(), (30 + 2*(128 // 3), 70))            
-            self.surface.blit(self[3].draw_card(), (40 + 3*(128 // 3), 70))
-            self.surface.blit(self[4].draw_card(), (50 + 4*(128 // 3), 70))
-
-        f1 = pygame.font.Font(None, 35)
-        line_1 = f1.render("Current bank: {0}".format(self.bank), 0, (50, 215, 50))
-
-        self.surface.blit(line_1, ((self.surface.get_width()-line_1.get_width())//2,10))
-
-
 
     def blind_bet(self, players, big_blind):
         bb = players[0]
--- poker_run.py
+++ poker_run.py
@@ -13,67 +13,59 @@ GAME STAGES:
 
 '''
 from poker_main import Player, StandardDeck, StandardBoard
+from random import randint
 
 comb_names = {
-	1:"High Card",
-	2:"Pair",
-	3:"Two pairs",
-	4:"Three of a kind",
-	5:"Straight",
-	6:"Flush",
-	7:"Full house",
-	8:"Four of a kind",
-	9:"Straight flush"}
+	1: "High Card",
+	2: "Pair",
+	3: "Two pairs",
+	4: "Three of a kind",
+	5: "Straight",
+	6: "Flush",
+	7: "Full house",
+	8: "Four of a kind",
+	9: "Straight flush"}
 
 deck = StandardDeck()
 board = StandardBoard()
-player1 = Player("One")
-you = Player("You")
-player3 = Player("Three")
-player4 = Player("Four")
-
-players_in_game = [you, player1]
-players_in_round = []
-run = True
-bb_list = [20,50,100,200,400,800]
-bb = bb_list[0]
+player_1 = Player("One")
 
 
-while run:
-	
-    board.game_round += 1
-    print([x.chip_amount for x in players_in_game])
-
-    if board.game_round%5 == 0:
-        bb = bb_list[board.game_round//5]
-    players_in_game.append(players_in_game.pop(0))
-    board.blind_bet(players_in_game, bb)
-
-    # PreFlop
-    deck.shuffle()
-
-    for x in players_in_game:
-    	x.hand.append(deck.pop(0))
-    	x.hand.append(deck.pop(0))
-    	for l in x.hand:
-    		l.showing = True
-
-    for x in players_in_game:
-    	print(x,x.hand)
-
-    print("Blinds:")
-    for x in players_in_game:
-    	print(x,x.current_bet)
-
-    for x in players_in_game:
-    	if x.current_bet < max([x.current_bet for x in players_in_game]):
-    		print("{0} needs to bet {1} to continue".format(x,max([x.current_bet for x in players_in_game])-x.current_bet))
-
-    you.decision(input("Decision:"),players_in_game,board)
-    break
-
-	
+if __name__ == "__main__":
+	counter = 0
+	while True:
+		counter += 1
+		deck = StandardDeck()
+		board = StandardBoard()
+		player_1 = Player("One")
+		player_2 = Player("Two")
+		deck.shuffle()
+		for card in deck:
+			card.showing = True
 
 
+		player_1.hand.append(deck.pop(randint(0,len(deck)-1)))
+		player_1.hand.append(deck.pop(randint(0,len(deck)-1)))
+		player_2.hand.append(deck.pop(randint(0,len(deck)-1)))
+		player_2.hand.append(deck.pop(randint(0,len(deck)-1)))
 
+		board.append(deck.pop(randint(0,len(deck)-1)))
+		board.append(deck.pop(randint(0,len(deck)-1)))
+		board.append(deck.pop(randint(0,len(deck)-1)))
+		board.append(deck.pop(randint(0,len(deck)-1)))
+		board.append(deck.pop(randint(0,len(deck)-1)))
 
+		print(counter)
+		print(player_1,player_1.hand)
+		print(player_2,player_2.hand)
+		print(board)
+
+		player_1.evaluate(board)
+		player_2.evaluate(board)
+
+		print("------------------------------------------------")
+		print(comb_names[player_1.combination[0]], "of", player_1.combination[1], "and", player_1.combination[2])
+		print("Kicker:", player_1.kicker[0])
+		print(comb_names[player_2.combination[0]], "of", player_2.combination[1], "and", player_2.combination[2])
+		print("Kicker:", player_2.kicker[0])
+		
