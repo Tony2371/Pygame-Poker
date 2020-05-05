5de9e40aadfcd5572641d045d25eb1d6a3d4536a
Anton <mrtony2371@gmail.com>
2020-05-05 12:03:57 +0300

    -removed check_bets function

--- poker_run.py
+++ poker_run.py
@@ -2,12 +2,12 @@
 TO DO LIST:
 - Kickers
 - Second high card for Two pairs and Full House
+-
 
 KNOWN BUGS:
 - max([x for x,y in zip(self.val_list,self.count_list) if y == 1])
 will be empty sequence if none of y == 1
 - Full house error if two sets
--
 '''
 from poker_main import Player, StandardDeck, StandardBoard
 from random import randint
@@ -20,46 +20,54 @@ player_3 = Player("Three")
 player_4 = Player("Four")
 
 players_in_game = [player_1,player_2,player_3,player_4]
-game_stages = []
-
-def check_bets(players):
-	while True:
-		for player in players:
-			if player.ingame and not player.answered:
-				print(player,", you'd better make a bet!")
-				player.decision(input("Decision:"),players_in_game,board)
-		if all([player.answered for player in players if player.ingame]):
-			break
-	print("Bets done!!!")
-
-	for player in players_in_game:
-		player.answered = False
 
 while True:
 	deck.reset()
 	if not deck.shuffled:
 		deck.shuffle()
+	# --------------------------PREFLOP-----------------------------------
+	board.blind_bet(players_in_game, board.current_blind)
 
 	for player in players_in_game:
 		player.hand.append(deck.pop(0))
 		player.hand.append(deck.pop(0))
-		print("--------",player,"--------")
-		print("Cards: ",player.hand)
-		print(player.chip_amount)
-		print(player.current_bet)
-	print("Bank:",board.bank)
 
-	board.blind_bet(players_in_game,100)
-	check_bets(players_in_game)
+	board.check_bets(players_in_game, board)
+	if board.check_winner(players_in_game,board):
+		break
 	print("Preflop ended!")
 
+	# --------------------------FLOP-----------------------------------
 	board.append(deck.pop(0))
 	board.append(deck.pop(0))
+	board.append(deck.pop(0))
+
+	print("Board: ",board)
+
+	board.check_bets(players_in_game, board)
+	board.check_winner(players_in_game, board)
+	print("Flop ended!")
+
+	# --------------------------TURN-----------------------------------
 	board.append(deck.pop(0))
 	print("Board: ",board)
 
-	check_bets(players_in_game)
-	print("Flop ended!")
+	board.check_bets(players_in_game, board)
+	board.check_winner(players_in_game, board)
+	print("Turn ended!")
 
+	# --------------------------RIVER-----------------------------------
+	board.append(deck.pop(0))
+	print("Board: ",board)
+
+	board.check_bets(players_in_game, board)
+	board.check_winner(players_in_game, board)
+	print("River ended!")
+
+	print("########################################")
+	print(board)
+	print("Bank:",board.bank)
+	for player in players_in_game:
+		print(player,"|",player.chip_amount,"|",player.hand,"|",player.combination)
 
 	break
