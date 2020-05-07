375de87640f4ad69bb980e8145916f468239f3a4
Anton <mrtony2371@gmail.com>
2020-05-07 12:38:01 +0300

    -added player table rotation

--- poker_run.py
+++ poker_run.py
@@ -1,15 +1,15 @@
 '''
 TO DO LIST:
+- Raise is possbile for minimum amount equal to blind
 - Bank split situation
 - All-in bank split
-- Bet limit mechanic (invalid bet if bet > chip amount)
-- Blinds increase
 
 KNOWN BUGS:
 - no bugs known for now
 
 SMALL ISSUES:
 - Four of a kind doesnt have kicker because it doesnt need
+- If (probably not only) "straight", combination shows kickers
 '''
 
 from poker_main import Player, StandardDeck, StandardBoard
@@ -35,14 +35,15 @@ while running:
 	if not deck.shuffled:
 		deck.shuffle()
 	# --------------------------PREFLOP-----------------------------------
-	board.blind_bet(players_in_game, board.current_blind)
+	players_in_game.append(players_in_game.pop(0))
+	board.blind_bet(players_in_game)
 
 	for player in players_in_game:
 		player.hand.append(deck.pop(0))
 		player.hand.append(deck.pop(0))
 
-	board.check_bets(players_in_game, board)
-	if board.check_winner(players_in_game,board):
+	board.check_bets(players_in_game)
+	if board.check_winner(players_in_game):
 		break
 	print("Preflop ended!")
 
@@ -53,35 +54,41 @@ while running:
 
 	print("Board: ",board)
 
-	board.check_bets(players_in_game, board)
-	board.check_winner(players_in_game, board)
+	board.check_bets(players_in_game)
+	board.check_winner(players_in_game)
 	print("Flop ended!")
 
 	# --------------------------TURN-----------------------------------
 	board.append(deck.pop(0))
 	print("Board: ",board)
 
-	board.check_bets(players_in_game, board)
-	board.check_winner(players_in_game, board)
+	board.check_bets(players_in_game)
+	board.check_winner(players_in_game)
 	print("Turn ended!")
 
 	# --------------------------RIVER-----------------------------------
 	board.append(deck.pop(0))
 	print("Board: ",board)
 
-	board.check_bets(players_in_game, board)
-	board.check_winner(players_in_game, board)
+	board.check_bets(players_in_game)
+	board.check_winner(players_in_game)
 	print("River ended!")
 
 	print("########################################")
+	print("Round:",board.game_round)
+	print("Blinds:",board.blinds_list[0],board.blinds_list[0]//2)
+	print("Blinds:",)
 	print(board)
-	print("Bank:",board.bank)
 	for player in players_in_game:
 		print(player,"|",player.chip_amount,"|",player.hand,"|",player.combination)
 
 	for player in players_in_game:
 		if player.chip_amount <= 0:
 			players_in_game.remove(player)
+			print(player, "eliminated!")
+
+	#xyz = input("Continue")
 
 	if len(players_in_game) == 1:
+		print(players_in_game[0], "won 'em all!")
 		running = False
